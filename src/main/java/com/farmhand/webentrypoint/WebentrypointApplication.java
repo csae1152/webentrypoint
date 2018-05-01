fdc3e7ed6384c8a4cb81f5f2f7b26956b8bd2122
HelmutSteiner <helmut.steinersti2@gmail.com>
2018-05-01 15:16:23 +0200

    Initial commit

---
+++ src/main/java/com/farmhand/webentrypoint/WebentrypointApplication.java
@@ -0,0 +1,12 @@
+package com.farmhand.webentrypoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebentrypointApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebentrypointApplication.class, args);
+    }
+}
